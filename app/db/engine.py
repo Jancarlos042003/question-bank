93425ce725408e72af7d0d98fa4d629d9ad70482
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-16 04:07:28 -0500

    add database connection setup and session management

---
+++ app/db/engine.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from app.core.config import settings
+
+USER = settings.DB_USER
+PASSWORD = settings.DB_PASSWORD
+HOST = settings.DB_HOST
+PORT = settings.DB_PORT
+DBNAME = settings.DB_NAME
+
+DATABASE_URL = f"postgresql+psycopg://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+engine = create_engine(url=DATABASE_URL, echo=True, future=True)
---
+++ app/db/session.py
@@ -0,0 +1,9 @@
+from sqlalchemy.orm import sessionmaker, Session
+from app.db.engine import engine
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    class_=Session
+)
