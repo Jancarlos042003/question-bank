0674109e14f0f2aa27d9b94563871bb5c83603a3
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-20 17:23:40 -0500

    add Optional in question_number and delete difficulty_id

--- app/models/question.py
+++ app/models/question.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.dialects.postgresql import JSONB
-from typing import List
+from typing import List, Optional
 from app.db.base import Base
 
 
@@ -9,19 +9,17 @@ class Question(Base):
     __tablename__ = "questions"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    question_number: Mapped[int] = mapped_column(Integer, nullable=False)
-    question_hash: Mapped[str] = mapped_column(String(64), unique=True, nullable=False)
-    statement: Mapped[dict] = mapped_column(JSONB, nullable=False)
-    solution: Mapped[dict] = mapped_column(JSONB, nullable=False)
-    difficulty_id: Mapped[int] = mapped_column(ForeignKey("difficulty.id"))
+    question_number: Mapped[Optional[int]] = mapped_column(Integer)
+    question_hash: Mapped[str] = mapped_column(String(64), unique=True)
+    statement: Mapped[dict] = mapped_column(JSONB)
+    solution: Mapped[dict] = mapped_column(JSONB)
     topic_id: Mapped[int] = mapped_column(ForeignKey("topics.id"))
-    exam_id: Mapped[int] = mapped_column(ForeignKey("exams.id"), nullable=False)
-    type_question_id: Mapped[int] = mapped_column(ForeignKey("types_questions.id"))
+    assessment_id: Mapped[int] = mapped_column(ForeignKey("assessments.id"))
+    question_type_id: Mapped[int] = mapped_column(ForeignKey("question_types.id"))
 
-    difficulty: Mapped["Difficulty"] = relationship()
     topic: Mapped["Topic"] = relationship()
-    exam: Mapped["Exam"] = relationship()
-    type_question: Mapped["TypeQuestion"] = relationship()
+    assessment: Mapped["Assessment"] = relationship()
+    question_type: Mapped["QuestionType"] = relationship()
 
     choices: Mapped[List["Choice"]] = relationship(
         back_populates="question",
