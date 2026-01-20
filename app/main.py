590e941b945241fe9b441158be9e15e96bce0ae4
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-20 17:24:42 -0500

    add lifespan, base_imports and routers

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI
 from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+import app.db.base_imports
 from app.db.base import Base
 from app.db.engine import engine
-import app.db.base_imports
+from app.routers.assessment import assessment_router
+from app.routers.question import question_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
     yield
 
+
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(question_router, prefix="/questions")
+app.include_router(assessment_router, prefix="/assessments")
 
 
 @app.get("/")
