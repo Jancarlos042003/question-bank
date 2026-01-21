56bc733db7311fde3f0274d623404c3c955305d0
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-21 11:33:57 -0500

    refactor(question): simplify creation logic and cleanup hashing

--- app/crud/question.py
+++ app/crud/question.py
@@ -12,26 +12,36 @@ from app.schemas.question import (
     QuestionCreate,
     StatementWithItems,
     MatchingStatement,
-    QuestionRead,
 )
 
 
 def create_question(db: Session, question: QuestionCreate):
     try:
         # 1. Crear pregunta
-        # Convertimos el modelo de pydantic en un dict
-        question_dict = question.model_dump(exclude={"choices"}, mode="json")
-        question_dict["question_hash"] = generate_question_hash(
-            statement=question.statement
+        question_hash = generate_question_hash(statement=question.statement)
+
+        new_question = Question(
+            question_number=question.question_number,
+            question_hash=question_hash,
+            statement=question.statement.model_dump(mode="json"),
+            solution=question.solution.model_dump(mode="json"),
+            topic_id=question.topic_id,
+            assessment_id=question.assessment_id,
+            question_type_id=question.question_type_id,
         )
 
-        new_question = Question(**question_dict)
         db.add(new_question)
         db.flush()  # obtiene ID sin commit
 
         # 2. Crear respuestas
         choices_orm = [
-            Choice(question_id=new_question.id, **choice.model_dump(mode="json"))
+            Choice(
+                label=choice.label,
+                content=choice.content,
+                is_correct=choice.is_correct,
+                image_url=choice.image_url,
+                question_id=new_question.id,
+            )
             for choice in question.choices
         ]
 
@@ -40,7 +50,7 @@ def create_question(db: Session, question: QuestionCreate):
         db.commit()
         db.refresh(new_question)
 
-        return QuestionRead.model_validate(new_question)
+        return new_question
     except SQLAlchemyError:
         db.rollback()
         raise HTTPException(status_code=500, detail="Error al crear la pregunta")
@@ -52,11 +62,6 @@ def get_all_questions(db: Session):
 
 
 def generate_question_hash(statement: Statement) -> str:
-    # payload = {
-    #     "statement": statement.text.strip().lower(),
-    #     "image_urls": sorted(statement.image_urls or [])
-    # }
-
     base = statement.text.strip().lower()
 
     if isinstance(statement, StatementWithItems):
@@ -74,33 +79,4 @@ def generate_question_hash(statement: Statement) -> str:
         )
         base += f"|{left}|{right}"
 
-    # # Verifica si tiene items
-    # if statement.items:
-    #     payload["items"] = sorted([
-    #         {
-    #             "id": item.id,
-    #             "content": item.content.strip().lower()
-    #         }
-    #         for item in statement.items
-    #     ], key=lambda item: item["content"])
-    #
-    # # Verifica si existen las columnas left_column y right_column
-    # if statement.left_column and statement.right_column:
-    #     payload["left_column"] = sorted([
-    #         {
-    #             "id": item.id,
-    #             "content": item.content.strip().lower()
-    #         }
-    #         for item in statement.left_column
-    #     ], key=lambda item: item["content"])
-    #
-    #     payload["right_column"] = sorted([
-    #         {
-    #             "id": item.id,
-    #             "content": item.content.strip().lower()
-    #         }
-    #         for item in statement.right_column
-    #     ], key=lambda item: item["content"])
-
-    # serialized = json.dumps(payload, sort_keys=True)
     return hashlib.sha256(base.encode("utf-8")).hexdigest()
