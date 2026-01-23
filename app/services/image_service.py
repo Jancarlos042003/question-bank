274cca843a020178748a96861980432636cb2c05
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-23 01:51:28 -0500

    feat: add method upload_images in image_service.py

--- app/services/image_service.py
+++ app/services/image_service.py
@@ -1,11 +1,18 @@
+import asyncio
+
 from app.ports.storage_port import StoragePort
 from fastapi import UploadFile
+from typing import List
+
 
 class ImageService:
     def __init__(self, storage: StoragePort):
         self.storage = storage
 
-    async def upload_image(self, image: UploadFile, storage_container_name: str, destination: str):
+    # Falta manejar las excepciones. Crear excepciones personalizadas.
+    async def upload_image(
+        self, image: UploadFile, storage_container_name: str, destination: str
+    ):
         image_bytes = await image.read()
 
         image_path = self.storage.upload_object_from_bytes(
@@ -16,6 +23,28 @@ class ImageService:
         )
 
         return image_path
+
+    async def upload_images(
+        self, images: List[UploadFile], storage_container_name: str, destination: str
+    ):
+        upload_images = []
+
+        if not images:
+            return []
+
+        for i in images:
+            image_bytes = await i.read()
+
+            image_path = self.storage.upload_object_from_bytes(
+                storage_container_name=storage_container_name,
+                data=image_bytes,
+                destination=destination + "/" + i.filename,
+                content_type=i.content_type,
+            )
+
+            upload_images.append(image_path)
+
+        return await asyncio.gather(*upload_images) # <-- OJO
 
     def generate_signature(self, storage_container_name: str, storage_object_name: str):
         url = self.storage.generate_signed_url(
