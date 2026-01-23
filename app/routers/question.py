b082506f21d0800508c4db7a4ec7373d708f7199
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-23 01:57:42 -0500

    refactor: the method add_question

--- app/routers/question.py
+++ app/routers/question.py
@@ -1,22 +1,45 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, UploadFile, File
 from sqlalchemy.orm import Session
 
-from app.crud.question import create_question, get_all_questions
+from app.core.config import settings
 from app.db.dep import get_db
+from app.infrastructure.gcp.storage_adapter import GCPStorageAdapter
 from app.schemas.question import QuestionCreate, QuestionRead
+from app.services.image_service import ImageService
+from app.services.question_service import QuestionService
 
 question_router = APIRouter()
+
+# Dependency injection
+gcp_storage = GCPStorageAdapter()
+image_service = ImageService(storage=gcp_storage)
+question_service = QuestionService(image_service)
 
 
 @question_router.post(
     "", response_model=QuestionRead, status_code=status.HTTP_201_CREATED
 )
-def add_question(question: QuestionCreate, db: Annotated[Session, Depends(get_db)]):
-    return create_question(db, question)
+async def add_question(
+        payload: QuestionCreate,
+        db: Annotated[Session, Depends(get_db)],
+        statement_images: list[UploadFile] | None = File(None),
+        choice_images: list[UploadFile] | None = File(None),
+        solution_images: list[UploadFile] | None = File(None),
+):
+    """Endpoint para crear una nueva pregunta."""
+    return await question_service.create_question(
+        db=db,
+        question=payload,
+        container_name=settings.CONTAINER_NAME,
+        statement_images=statement_images,
+        choice_images=choice_images,
+        solution_images=solution_images,
+    )
 
 
 @question_router.get("", response_model=list[QuestionRead])
 def all_questions(db: Annotated[Session, Depends(get_db)]):
-    return get_all_questions(db)
+    """Endpoint para obtener todas las preguntas."""
+    return question_service.get_all_questions(db)
