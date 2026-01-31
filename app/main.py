2351e3def12dee9b95757e0a68679860b5139ffd
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-31 06:23:54 -0500

    Add CORS middleware for development

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 
+from app.core.middleware import register_middleware
 from app.db.engine import
 from app.routers.assessment import assessment_router
 from app.routers.question import question_router
 
 app = FastAPI()
+
+register_middleware(app)
 
 app.include_router(question_router, prefix="/questions")
 app.include_router(assessment_router, prefix="/assessments")
