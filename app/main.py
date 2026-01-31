2f25a78ecccbbdc2084e4825822efdb7409bf22a
Jancarlos042003 <U21302940@utp.edu.pe>
2026-01-30 20:30:29 -0500

    Remove lifespan and metadata creation

--- app/main.py
+++ app/main.py
@@ -1,21 +1,10 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 
-import app.db.base_imports
-from app.db.base import Base
-from app.db.engine import engine
+from app.db.engine import
 from app.routers.assessment import assessment_router
 from app.routers.question import question_router
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    Base.metadata.create_all(bind=engine)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.include_router(question_router, prefix="/questions")
 app.include_router(assessment_router, prefix="/assessments")
