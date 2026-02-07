db693a1c90f5764171be2c9f6b03673740a23e4c
Jancarlos042003 <U21302940@utp.edu.pe>
2026-02-06 20:43:11 -0500

    Refactor question_areas to declarative model

--- app/models/question_areas.py
+++ app/models/question_areas.py
@@ -1,10 +1,13 @@
-from sqlalchemy import Column, ForeignKey, Table
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base import Base
 
-question_areas = Table(
-    "question_areas",
-    Base.metadata,
-    Column("question_id", ForeignKey("questions.id"), primary_key=True),
-    Column("area_id", ForeignKey("areas.id"), primary_key=True)
-)
+
+class QuestionAreas(Base):
+    __tablename__ = "question_areas"
+
+    question_id: Mapped[int] = mapped_column(
+        ForeignKey("questions.id"), primary_key=True
+    )
+    area_id: Mapped[int] = mapped_column(ForeignKey("areas.id"), primary_key=True)
