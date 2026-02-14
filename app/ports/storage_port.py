2856fe0eb090c1472cd05c53760345358ea72869
Jancarlos042003 <u21302940@utp.edu.pe>
2026-02-14 18:11:40 -0500

    Fix missing abstractmethod in StoragePort

--- app/ports/storage_port.py
+++ app/ports/storage_port.py
@@ -4,7 +4,9 @@ from abc import ABC, abstractmethod
 class StoragePort(ABC):
 
     @abstractmethod
-    def generate_signed_url(self, storage_container_name: str,storage_object_name: str) -> str:
+    def generate_signed_url(
+            self, storage_container_name: str, storage_object_name: str
+    ) -> str:
         """
         Genera una URL firmada para lectura
         :param storage_container_name: Nombre del bucket o contenedor donde se encuentra el objeto.
@@ -13,12 +15,13 @@ class StoragePort(ABC):
         """
         pass
 
+    @abstractmethod
     def upload_object_from_bytes(
-        self,
-        storage_container_name: str,
-        data: bytes,
-        destination: str,
-        content_type: str | None = None,
+            self,
+            storage_container_name: str,
+            data: bytes,
+            destination: str,
+            content_type: str | None = None,
     ) -> str:
         """
         Sube un archivo al bucket.
