be66e7b2dd0b057c0c4ea27b6248cc5ec1b8b9ae
Jancarlos042003 <u21302940@utp.edu.pe>
2026-02-19 17:59:12 -0500

    Add cascade delete to question_areas association table

--- app/models/question_areas.py
+++ app/models/question_areas.py
@@ -1,11 +1,20 @@
 from sqlalchemy import Column, ForeignKey, Table
-from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base import Base
 
 question_areas = Table(
     "question_areas",
     Base.metadata,
-    Column("question_id", ForeignKey("questions.id"), primary_key=True, index=True),
-    Column("area_id", ForeignKey("areas.id"), primary_key=True, index=True),
+    Column(
+        "question_id",
+        ForeignKey("questions.id", ondelete="CASCADE"),
+        primary_key=True,
+        index=True,
+    ),
+    Column(
+        "area_id",
+        ForeignKey("areas.id", ondelete="CASCADE"),
+        primary_key=True,
+        index=True,
+    ),
 )
